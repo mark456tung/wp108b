a12508c8402a91238938ae992abe06572a2b2c56
mark456tung <mark456tung@gmail.com>
2020-05-24 10:22:30 +0800

    modifyproject1

---
+++ homework/homework3/10!.js
@@ -0,0 +1,6 @@
+i=10,sum=1;
+while(i>=1){
+    sum=sum*i;
+    i=i-1;
+}
+console.log("10!=",sum);
---
+++ homework/homework3/prime.js
@@ -0,0 +1,15 @@
+x=3,y=7,z=0
+function isPrime(i){
+    for(j=2;j<i;j++){
+        if(i%j==0){
+            return false;
+        }
+    }
+    return true;
+}
+for(i=x;i<=y;i++){
+    if(isPrime(i)){
+        z+=1;
+    }
+}
+console.log("countPrime(%d,%d),have %d prime",x,y,z);
